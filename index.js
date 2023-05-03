a8fb036ba4770af73e5cff98e7b7e3cb7b1aaffc
mominul <ulhouqemomin39@gmail.com>
2023-05-03 15:35:58 +0600

    updated json file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 4000;
 const cors = require('cors');
 
 const datas = require("./data/datas.json");
+const details = require('./data/userdetails.json')
 
 app.use(cors());
 
@@ -11,29 +12,46 @@ app.get('/', (req, res) => {
   res.send('Server Is running!')
 })
 
+
+
+
 app.get('/datas', (req, res) => {
   res.send(datas);
 })
+
+// main data loading
+app.get('/datas/:id', (req,res)=>{
+  const id = parseInt(req.params.id);
+  if(id === 0){
+    res.send(details)
+  }
+})
+
+
+// Details data
+app.get("/details", (req, res) =>{
+  res.send(details)
+})
+
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const seletedProfile = details.find((n) => n.id === id);
+  res.send(seletedProfile)
+})
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
 
-/* 
-
-
-https://ibb.co/Vv2hnww
-
-https://ibb.co/swzv1JB
-
-https://ibb.co/L9scgsN
-
-https://ibb.co/FXD31Dp
-
-
-https://ibb.co/rv8RbQB
-
-https://ibb.co/pZNHPJ9
-
-*/
